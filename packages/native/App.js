47a0ea2009d800c7d7fb2e2cd4022802ee0c8e37
JPBlancoDB <jpblancodb@gmail.com>
2018-04-08 16:41:48 -0300

    Add map component with markers.

--- packages/native/App.js
+++ packages/native/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import markersService from './services/markers';
+import Map from './components/Map';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Map markers={markersService.getMarkers} />
       </View>
     );
   }
